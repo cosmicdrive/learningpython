591e4633178ff1db192dc24abad6f244b9274679
Mr. Palmer <palmer.howard@gmail.com>
2025-03-27 13:05:03 -0600

    Add files via upload

---
+++ ADD NUMBERS.py
@@ -0,0 +1,12 @@
+Python 3.13.2 (v3.13.2:4f8bb3947cf, Feb  4 2025, 11:51:10) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # This program adds two numbers
+>>> num1  = 1
+>>> num2 = 34
+>>> 
+>>> # Add two numbers
+>>> sum = num1 + num2
+>>> 
+>>> #Display the sum
+>>> print("The sum total of the numbers is:", sum)
+The sum total of the numbers is: 35
